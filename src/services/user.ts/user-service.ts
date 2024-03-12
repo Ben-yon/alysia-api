9be287f40fdbe99bb556afabb8787d76db630bcd
Benjamin Ikeh <b.ikeh@yahoo.com>
2024-03-12 12:49:03 +0000

    final commit

--- src/services/user.ts/user-service.ts
+++ src/services/user.ts/user-service.ts
@@ -1,10 +1,13 @@
 import User from "../../models"
 import user from "../../models"
+import db from "../../../config/db"
 
 export default {
  
     async register(user: User): Promise<any> {
-        
+        await db.user.create({
+            
+        })
     }   
 
 }
